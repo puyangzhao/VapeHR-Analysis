be620c3e3864116113736b6fb65c9da5b2ed8bed
PP Zhao <64766594+puyangzhao@users.noreply.github.com>
2024-08-20 21:48:48 -0500

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -2,13 +2,13 @@ import pandas as pd
 
 def load_event_data(filepath):
     eventdata = pd.read_csv(filepath)
-    eventdata['isoDate'] = [date[:-6] for date in eventdata['isoDate']]
-    eventdata['isoDate'] = pd.to_datetime(eventdata['isoDate'])
+    eventdata['Date'] = [date[:-6] for date in eventdata['Date']]
+    eventdata['Date'] = pd.to_datetime(eventdata['Date'])
     return eventdata
 
 def fetch_data(uid, eventdata):
     eventdata_ = eventdata[eventdata['UID'] == uid]
-    hr_data = pd.read_csv(f"{uid}_imputed_hr.csv")
-    hr_data['dt'] = pd.to_datetime(hr_data['dt'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
-    hr_nosleep = hr_data[hr_data['sleepornot'] == 0]
+    hr_data = pd.read_csv(f"{uid}_data.csv")
+    hr_data['dt'] = pd.to_datetime(hr_data['time'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
+    hr_nosleep = hr_data[hr_data['is_sleep'] == 0]
     return eventdata_, hr_data, hr_nosleep
