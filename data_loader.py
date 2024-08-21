ff47be61ca2829a8b43bd4aa67778cbb84a301da
PP Zhao <64766594+puyangzhao@users.noreply.github.com>
2024-08-20 22:16:39 -0500

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -8,7 +8,7 @@ def load_event_data(filepath):
 
 def fetch_data(uid, eventdata):
     eventdata_ = eventdata[eventdata['UID'] == uid]
-    hr_data = pd.read_csv(f"{uid}_data.csv")
+    hr_data = pd.read_csv(f"{id}_data.csv")
     hr_data['dt'] = pd.to_datetime(hr_data['time'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
     hr_nosleep = hr_data[hr_data['is_sleep'] == 0]
     return eventdata_, hr_data, hr_nosleep
