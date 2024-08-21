9d92a4f3b3d1001ba6dd0cbbd733f054df8b78be
PP Zhao <64766594+puyangzhao@users.noreply.github.com>
2024-08-20 22:17:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ from regression_analysis import perform_regression_analysis, plot_regression_res
 # eventdata = pd.read_csv('path_to_eventdata.csv')
 
 # Define the list of user IDs to process
-# filtered_uids = ['user1', 'user2', ...]
+# filtered_ids = ['user1', 'user2', ...]
 
 # Process all users to compute matrix profiles
-all_matrix_profiles, hr_nosleep_df = process_all_users(filtered_uids, eventdata)
+all_matrix_profiles, hr_nosleep_df = process_all_users(filtered_ids, eventdata)
 
 # Create annotation vectors for all users
-annotation_vectors = create_annotation_vectors(eventdata, all_matrix_profiles, filtered_uids)
+annotation_vectors = create_annotation_vectors(eventdata, all_matrix_profiles, filtered_ids)
 
 # Correct the matrix profiles using annotation vectors
 corrected_matrix_profiles, all_corrected_profiles = correct_matrix_profiles(all_matrix_profiles, annotation_vectors)
