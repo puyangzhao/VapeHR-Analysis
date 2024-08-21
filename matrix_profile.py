c6afc7a2705b1b38017d9270ce3eccabd827aca9
PP Zhao <64766594+puyangzhao@users.noreply.github.com>
2024-08-20 22:17:38 -0500

    Update matrix_profile.py

--- matrix_profile.py
+++ matrix_profile.py
@@ -17,8 +17,8 @@ def compute_matrix_profile(data, window_size):
         return None
     # Ensure that time information is preserved after computing the matrix profile
     stumpy.config.STUMPY_EXCL_ZONE_DENOM = 0.5
-    # Calculate the matrix profile for 'hr_imp' column with the specified window size, without normalization
-    mp = stumpy.stump(data['hr_imp'], m=window_size, normalize=False)
+    # Calculate the matrix profile for 'hr' column with the specified window size, without normalization
+    mp = stumpy.stump(data['hr'], m=window_size, normalize=False)
     # Create a DataFrame with the matrix profile and the corresponding time values
     mp_df = pd.DataFrame({
         'time': data['dt'][0:len(mp[:, 0])],  # Preserve the time column
