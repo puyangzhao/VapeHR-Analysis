69f2d0123a4d21a5ecb5077f6dd34680762cd24f
PP Zhao <64766594+puyangzhao@users.noreply.github.com>
2024-10-10 11:24:17 -0500

    Update data_loading.R

--- sleep detection/data_loading.R
+++ sleep detection/data_loading.R
@@ -1,12 +1,12 @@
 # data_loading.R
 
 library(dplyr)
-library(lubridate)
+library(purrr)
 
 load_hr_data <- function(ids, data_path) {
   data_list <- list()
   for (id in ids) {
-    hr_filename <- file.path(data_path, paste0(id, "data.csv"))
+    hr_filename <- paste0(data_path, id, "_imputed_hr.csv")
     data_list[[id]] <- read.csv(hr_filename)
   }
   return(data_list)
@@ -15,10 +15,11 @@ load_hr_data <- function(ids, data_path) {
 load_sleep_data <- function(ids, report_path) {
   sleep_sessions_list <- list()
   for (id in ids) {
-    filename <- file.path(report_path, paste0(id, "_sleepreport.csv"))
+    filename <- paste0(report_path, id, "_sleepreport.csv")
     sleep_data <- read.csv(filename)
     sleep_data$isoDate <- as.POSIXct(sleep_data$isoDate, format = "%Y-%m-%dT%H:%M:%OS", tz = "UTC")
-    sleep_sessions_list[[id]] <- sleep_data
+    sleep_sessions <- extract_sleep_sessions(sleep_data)
+    sleep_sessions_list[[id]] <- sleep_sessions
   }
   return(sleep_sessions_list)
 }
