bdb8f51c7f53eaefdc6dd5f5070a0cf4f23724be
PP Zhao <64766594+puyangzhao@users.noreply.github.com>
2024-10-10 11:24:50 -0500

    Update sleep_detection.R

--- sleep detection/sleep_detection.R
+++ sleep detection/sleep_detection.R
@@ -25,3 +25,70 @@ extract_sleep_sessions <- function(sleep_data) {
   }
   return(sleep_sessions)
 }
+
+process_hr_data <- function(hr_data1) {
+  hr_data1$date <- as.Date(hr_data1$date)
+  hr_data1$sleepornot <- rep(0, nrow(hr_data1))
+  return(hr_data1)
+}
+
+detect_sleep <- function(hr_data1) {
+  two_day_combinations <- hr_data1 %>%
+    group_by(date) %>%
+    summarise(hr_list = list(hr_imp), .groups = 'drop') %>%
+    mutate(next_hr_list = lead(hr_list)) %>%
+    na.omit() %>%
+    mutate(combined_hr = map2(hr_list, next_hr_list, c)) %>%
+    select(date, combined_hr)
+  
+  results <- list()
+  for (i in seq_along(two_day_combinations$combined_hr)) {
+    hr_data <- two_day_combinations$combined_hr[[i]]
+    date_range <- two_day_combinations$date[i]
+    
+    s <- susie_trendfilter(hr_data, scaled_prior_variance=0, L=7)
+    yres <- predict(s)
+    
+    actual_lengths <- table(hr_data1$date[hr_data1$date %in% c(date_range, date_range + 1)])
+    first_day_length <- actual_lengths[1]
+    
+    inx <- which(yres[1:first_day_length] >= min(yres[1:first_day_length]) & yres[1:first_day_length] <= min(yres[1:first_day_length]) + 9.5)
+    
+    # 更新结果
+    result <- list(date = date_range, yres = yres, sleepornot = rep(0, length(hr_data)))
+    result$sleepornot[inx] <- 1
+    results[[i]] <- result
+  }
+  return(results)
+}
+
+update_hr_data <- function(hr_data1, results) {
+  for (res in results) {
+    date_range <- res$date
+    first_day_length <- length(res$sleepornot[1:first_day_length])
+    hr_data1$sleepornot[hr_data1$date == date_range] <- res$sleepornot[1:first_day_length]
+    hr_data1$fitted_hr[hr_data1$date == date_range] <- res$yres[1:first_day_length]
+  }
+  hr_data1 <- hr_data1 %>%
+    mutate(
+      dt = as.POSIXct(dt, format="%Y-%m-%dT%H:%M:%S", tz = "UTC"),
+      date = as.Date(date),
+      sleepornot = as.factor(sleepornot)
+    )
+  return(hr_data1)
+}
+
+update_sleep_report <- function(hr_data1, sleep_sessions_df) {
+  sleep_sessions_df <- mutate(sleep_sessions_df,
+                              Start = as.POSIXct(Start, format="%Y-%m-%d %H:%M:%S", tz = "UTC"),
+                              End = as.POSIXct(End, format="%Y-%m-%d %H:%M:%S", tz = "UTC"),
+                              ymin = 40,
+                              ymax = 120)
+  hr_data1$sleep_report <- 0
+  for (i in 1:nrow(sleep_sessions_df)) {
+    start_time <- sleep_sessions_df$Start[i]
+    end_time <- sleep_sessions_df$End[i]
+    hr_data1$sleep_report[hr_data1$dt >= start_time & hr_data1$dt <= end_time] <- 1
+  }
+  return(hr_data1)
+}
