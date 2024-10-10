cf9453705bf2431cd8c6eaebee11e5b6ab0028cf
PP Zhao <64766594+puyangzhao@users.noreply.github.com>
2024-10-10 11:23:41 -0500

    Update main.R

--- sleep detection/main.R
+++ sleep detection/main.R
@@ -1,4 +1,4 @@
-# main.R
+# main_script.R
 
 source("R/data_loading.R")
 source("R/sleep_detection.R")
@@ -9,17 +9,28 @@ data_path <- "data/"
 report_path <- "reports/"
 output_path <- "results/"
 
-hr_data_list <- load_hr_data(ids, data_path)
-sleep_data_list <- load_sleep_data(ids, report_path)
+library(dplyr)
+library(purrr)
+library(susieR)
+library(Rfast)
+library(lubridate)
+library(ggplot2)
+library(patchwork)
+library(scales)
 
-for (id in ids) {
-  hr_data1 <- hr_data_list[[id]]
-  sleep_data <- sleep_data_list[[id]]
-  
-  sleep_sessions <- extract_sleep_sessions(sleep_data)
+IDs <- paste0('ID', 1:10)
+
+data_list <- load_hr_data(IDs, data_path)
+sleep_sessions_list <- load_sleep_data(IDs, report_path)
+
+for (id in IDs) {
+  hr_data1 <- data_list[[id]]
+  sleep_sessions_df <- sleep_sessions_list[[id]]
+  hr_data1 <- process_hr_data(hr_data1)
+  results <- detect_sleep(hr_data1)
+  hr_data1 <- update_hr_data(hr_data1, results)
+  hr_data1 <- update_sleep_report(hr_data1, sleep_sessions_df)
   save_hr_data(hr_data1, id, output_path)
-
-  plots <- plot_sleep_detection(hr_data1, sleep_sessions, id, date_range, date_labels)
-  save_plot(plots$p1, id, output_path, "p1")
-  save_plot(plots$p2, id, output_path, "p2")
+  id_index <- match(id, IDs)
+  create_and_save_plots(hr_data1, sleep_sessions_df, id, output_path, id_index)
 }
